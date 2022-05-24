e27f97ad03cd2efc703f95a544d2f9e3f0085983
tbauz <tbauzil@gmail.com>
2022-05-24 14:40:21 +0200

    commit test deploiement

--- AccManager/src/main/java/com/example/AccManager/account/AccountController.java
+++ AccManager/src/main/java/com/example/AccManager/account/AccountController.java
@@ -1,6 +1,7 @@
 package com.example.AccManager.account;
 
 import org.springframework.web.bind.annotation.*;
+import static com.googlecode.objectify.ObjectifyService.ofy;
 
 @RestController
 @RequestMapping("/employee")
@@ -8,9 +9,21 @@ public class AccountController {
 
     @GetMapping("/add")
     public @ResponseBody
-    Object addAccount(@RequestParam(value="nom", defaultValue="XXX") String nom, @RequestParam(value="prenom", defaultValue = "YYY") String prenom,
+    int addAccount(@RequestParam(value="nom", defaultValue="XXX") String nom, @RequestParam(value="prenom", defaultValue = "YYY") String prenom,
                       @RequestParam(value="num", defaultValue = "0") int num, @RequestParam(value="risk", defaultValue = "...") String risk){
         Account account = new Account(nom, prenom, num, risk);
+        try{
+            ofy().save().entity(account).now();
+        } catch (Exception e){
+            throw new RuntimeException("Le compte n'a pas pu être ajouté");
+        }
+
+        return 200;
+    }
+
+    @GetMapping("/test")
+    public @ResponseBody
+    int addAccount(){
         return 200;
     }
 
