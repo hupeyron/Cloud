2b5eeaa640f00cf11191b5a10c2ad21869ffba24
tbauz <tbauzil@gmail.com>
2022-05-25 12:17:00 +0200

    add ok

--- AccManager/src/main/java/com/example/AccManager/account/AccountController.java
+++ AccManager/src/main/java/com/example/AccManager/account/AccountController.java
@@ -1,17 +1,26 @@
 package com.example.AccManager.account;
 
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
 @RestController
 @RequestMapping("/employee")
 public class AccountController {
 
-    @GetMapping("/add")
+    @GetMapping("/add/{nom}")
     public @ResponseBody
-    int addAccount(@RequestParam(value="nom", defaultValue="XXX") String nom, @RequestParam(value="prenom", defaultValue = "YYY") String prenom,
+    int addAccount(@PathVariable String nom, @RequestParam(value="prenom", defaultValue = "PRENOM") String prenom,
                       @RequestParam(value="num", defaultValue = "0") int num, @RequestParam(value="risk", defaultValue = "...") String risk){
-        Account account = new Account("123123", nom, prenom, num, risk);
+        Random rand = new Random(); //instance of random class
+        int upperbound = 9999999;
+        //generate random values from 0-9999998
+        int int_random = rand.nextInt(upperbound);
+
+        String id = Integer.toString(int_random);
+        Account account = new Account(id, nom, prenom, num, risk);
         try{
             ofy().save().entity(account).now();
         } catch (Exception e){
