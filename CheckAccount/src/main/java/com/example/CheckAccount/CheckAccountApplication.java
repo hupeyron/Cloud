9c285f8a1d04bb81166755194d3678e4097a5ad1
tbauz <tbauzil@gmail.com>
2022-05-27 12:38:29 +0200

    [CheckAccount] - set up du service

---
+++ CheckAccount/src/main/java/com/example/CheckAccount/CheckAccountApplication.java
@@ -0,0 +1,13 @@
+package com.example.CheckAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckAccountApplication.class, args);
+	}
+
+}
---
+++ CheckAccount/src/test/java/com/example/CheckAccount/CheckAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CheckAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
