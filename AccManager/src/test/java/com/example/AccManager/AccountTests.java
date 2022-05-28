797f0c135236924255d6fa71a44a239de93ee803
tbauz <tbauzil@gmail.com>
2022-05-28 12:10:32 +0200

    test

--- AccManager/src/test/java/com/example/AccManager/AccountTests.java
+++ AccManager/src/test/java/com/example/AccManager/AccountTests.java
@@ -1,2 +1,20 @@
-package com.example.AccManager;public class AccountTests {
+package com.example.AccManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AccountTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testTest(){
+        assertEquals("200", restTemplate.getForObject("http://localhost:8080/account/test", String.class));
+    }
 }
